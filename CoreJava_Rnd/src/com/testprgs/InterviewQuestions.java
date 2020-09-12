ef29dcb38847b7b65a6c0486d0c4c61008316e7a
Amit Kumar <asinha.kumar@gmail.com>
2020-09-12 09:59:47 +0530

    Updated

--- CoreJava_Rnd/src/com/testprgs/InterviewQuestions.java
+++ CoreJava_Rnd/src/com/testprgs/InterviewQuestions.java
@@ -5,7 +5,7 @@ public class InterviewQuestions {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hi Amit, Welcome to core Java...");
+		System.out.println("Hi Amit, Welcome for Core Java Test Preparation...");
 		
 		testReverseString();
 
