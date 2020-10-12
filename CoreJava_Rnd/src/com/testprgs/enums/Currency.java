3891819434a1f3e8f388be3106ded7027b8d3c75
Amit Kumar <asinha.kumar@gmail.com>
2020-10-12 18:43:40 +0530

    Updated

--- CoreJava_Rnd/src/com/testprgs/enums/Currency.java
+++ CoreJava_Rnd/src/com/testprgs/enums/Currency.java
@@ -2,16 +2,20 @@ package com.testprgs.enums;
 
 public enum Currency {
 
-	RUPEE(5), DOLLAR(10), RUBEL(15), YEN(20);
+	RUPEE(5),
+	DOLLAR(10),
+	RUBEL(15),
+	YEN(20);
 
 	private int value;
+	
+	private Currency(int value) {
+		this.value = value;
+	}
 
 	public int getValue() {
 		return value;
 	}
 
-	private Currency(int value) {
-		this.value = value;
-	}
-
+	
 }
