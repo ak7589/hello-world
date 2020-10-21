4e0a906c029213e3d0f12f25b2ebecbdb4bbcd15
Amit Kumar <asinha.kumar@gmail.com>
2020-10-21 15:02:08 +0530

    Updated

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/entity/EmployeeErrorResponse.java
@@ -0,0 +1,22 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+public class EmployeeErrorResponse {
+	
+	private int resStatus;
+	private String resMessage;
+	
+	public int getResStatus() {
+		return resStatus;
+	}
+	public void setResStatus(int resStatus) {
+		this.resStatus = resStatus;
+	}
+	public String getResMessage() {
+		return resMessage;
+	}
+	public void setResMessage(String resMessage) {
+		this.resMessage = resMessage;
+	}
+	
+
+}
