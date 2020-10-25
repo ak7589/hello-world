cf356ecdc5d5fb4ccc2953b3718e57a7a2ff21e1
ak7589 <asinha.kumar@gmail.com>
2020-10-25 13:10:25 +0530

    Updated

---
+++ CoreJava_Rnd/src/com/testprgs/designpattern/decorator/Icecream.java
@@ -0,0 +1,7 @@
+package com.testprgs.designpattern.decorator;
+
+public interface Icecream {
+	
+	public void makeIcecream();
+
+}
---
+++ CoreJava_Rnd/src/com/testprgs/designpattern/decorator/TestDecorator.java
@@ -0,0 +1,13 @@
+package com.testprgs.designpattern.decorator;
+
+public class TestDecorator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SimpleIcecream icecream = new SimpleIcecream();
+		
+
+	}
+
+}
